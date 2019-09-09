e02131e94a172ac7e3fede02aa3178b96126ae35
wasim2263 <abdullahwasim42@gmail.com>
2019-09-09 22:36:34 +0600

    base model done

--- remote_tutor/tuition/models.py
+++ remote_tutor/tuition/models.py
@@ -1,22 +1,29 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
 from model_utils.models import TimeStampedModel, SoftDeletableModel
 
 from remote_tutor.student.models import Student
-from remote_tutor.tutor.models import Tutor
+from remote_tutor.tutor.models import Tutor, Subject, TUITION_TYPE
 
 TUITION_STATUS_CHOICES = (
-    ('active', _('active')),
-    ('inactive', _('inactive')),
-    ('finished', _('finished')),
-    ('disputed', _('disputed')),
+    ("active", _("active")),
+    ("inactive", _("inactive")),
+    ("finished", _("finished")),
+    ("disputed", _("disputed")),
 )
 
 
 class Tuition(SoftDeletableModel, TimeStampedModel):
     tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE, null=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
-    status = models.CharField(max_length=20, choices=TUITION_STATUS_CHOICES, default='active')
+    status = models.CharField(max_length=20, choices=TUITION_STATUS_CHOICES, default="active")
+    tuition_type = models.CharField(max_length=20, choices=TUITION_TYPE, default="monthly")
+    lectures = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(28)], default=1)
+    subject = models.ManyToManyField(Subject)
 
-# class Booking(SoftDeletableModel, TimeStampedModel):
+
+class Lecture(SoftDeletableModel, TimeStampedModel):
+    tuition = models.ForeignKey(Tuition, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=(("started", _("started")),
+                                                      ("finished", _("finished")),), default="started")
--- remote_tutor/tutor/models.py
+++ remote_tutor/tutor/models.py
@@ -1,6 +1,36 @@
 from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from model_utils.models import TimeStampedModel, SoftDeletableModel
+
+
+TUITION_TYPE = (
+    ("monthly", "Monthly"),
+    ("class_number", "Class Number"),
+    ("instant", "Instant"),
+)
+
+CLASS_LEVELS = (
+    ("any", "Any"),
+    ("class_1", "Class 1"),
+    ("class_2", "Class 2"),
+    ("class_3", "Class 3"),
+    ("class_4", "Class 4"),
+    ("class_5", "Class 5"),
+    ("class_6", "Class 6"),
+    ("class_7", "Class 7"),
+    ("class_8", "Class 8"),
+    ("class_9", "Class 9"),
+    ("class_10", "Class 10"),
+    ("class_11", "Class 11"),
+    ("class_12", "Class 12"),
+    ("o_level", "O Level"),
+    ("a_level", "A Level"),
+    ("ba", "BA"),
+    ("bsc", "BSc"),
+    ("bcom", "BCom"),
+    ("masters", "Masters")
+)
 
 
 class Category(SoftDeletableModel, TimeStampedModel):
@@ -13,6 +43,13 @@ class Category(SoftDeletableModel, TimeStampedModel):
 class Subject(SoftDeletableModel, TimeStampedModel):
     name = models.CharField(max_length=255, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(SoftDeletableModel, TimeStampedModel):
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
@@ -60,7 +97,7 @@ class Occupation(SoftDeletableModel, TimeStampedModel):
 
 class Tutor(SoftDeletableModel, TimeStampedModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-    subject = models.ManyToManyField(Subject)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, null=True, blank=True)
     current_occupation = models.ForeignKey(Occupation, on_delete=models.PROTECT, null=True)
     current_institute = models.CharField(max_length=255, null=True, blank=True,
                                          help_text="current job or service institute.")
@@ -68,5 +105,12 @@ class Tutor(SoftDeletableModel, TimeStampedModel):
     college = models.ForeignKey(College, on_delete=models.PROTECT, null=True, blank=True)
     school = models.ForeignKey(School, on_delete=models.PROTECT, null=True)
     verified = models.BooleanField(default=False)
-    # verified_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True,
-    #                                 related_name='verifier')
+    verified_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True,
+                                    related_name="tutor_verifier")
+
+
+class Preference(SoftDeletableModel, TimeStampedModel):
+    tutor = models.OneToOneField(Tutor, on_delete=models.CASCADE)
+    tuition_type = models.CharField(max_length=20, choices=TUITION_TYPE, default="monthly")
+    class_level = ArrayField(models.CharField(max_length=25, choices=CLASS_LEVELS, default="any"))
+    subject = models.ManyToManyField(Subject)
